bbdf4471755764c43a762e02b36a11e7ce87ce16
Abel Pinheiro de Figueiredo <abelpinheiro95@gmail.com>
2018-07-04 23:52:05 -0300

    adicionado método releaseMediaPlayer para melhor eficiência de memória no aplicativo e para repetir o áudio ao clicar antes de acabar o áudio anterior.
    travado aplicativo para portrait-only

--- app/src/main/java/abelpinheiro/github/io/megumin/MainActivity.java
+++ app/src/main/java/abelpinheiro/github/io/megumin/MainActivity.java
@@ -8,8 +8,15 @@ import android.widget.ImageView;
 import android.widget.LinearLayout;
 
 public class MainActivity extends AppCompatActivity {
-    MediaPlayer mediaPlayer;
+    private MediaPlayer mMediaPlayer;
     int numberImage = 0;
+
+    private MediaPlayer.OnCompletionListener mCompletionListener = new MediaPlayer.OnCompletionListener() {
+        @Override
+        public void onCompletion(MediaPlayer mediaPlayer) {
+            releaseMediaPlayer();
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +25,6 @@ public class MainActivity extends AppCompatActivity {
         numberImage = 0;
         final LinearLayout layout = findViewById(R.id.layout);
         final ImageView meguminImage = findViewById(R.id.megumin_image);
-        mediaPlayer = MediaPlayer.create(this, R.raw.explosion_3);
         meguminImage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -26,22 +32,48 @@ public class MainActivity extends AppCompatActivity {
                     case 0:
                         layout.setPadding(0,0,0,0);
                         meguminImage.setImageResource(R.drawable.megumin01);
-                        mediaPlayer.start();
+                        releaseMediaPlayer();
+                        mMediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.explosion_3);
+                        mMediaPlayer.start();
+                        mMediaPlayer.setOnCompletionListener(mCompletionListener);
                         numberImage = 1;
                         break;
                     case 1:
                         meguminImage.setImageResource(R.drawable.megumin03);
                         numberImage = 2;
-                        mediaPlayer.start();
+                        releaseMediaPlayer();
+                        mMediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.explosion_3);
+                        mMediaPlayer.start();
+                        mMediaPlayer.setOnCompletionListener(mCompletionListener);
                         break;
                     case 2:
                         layout.setPadding(50,50,50,50);
                         meguminImage.setImageResource(R.drawable.megumin02);
                         numberImage = 0;
-                        mediaPlayer.start();
+                        releaseMediaPlayer();
+                        mMediaPlayer = MediaPlayer.create(MainActivity.this, R.raw.explosion_3);
+                        mMediaPlayer.start();
+                        mMediaPlayer.setOnCompletionListener(mCompletionListener);
                         break;
                 }
             }
         });
+    }
+
+    /**
+     * Clean up the media player by releasing its resources.
+     */
+    private void releaseMediaPlayer() {
+        // If the media player is not null, then it may be currently playing a sound.
+        if (mMediaPlayer != null) {
+            // Regardless of the current state of the media player, release its resources
+            // because we no longer need it.
+            mMediaPlayer.release();
+
+            // Set the media player back to null. For our code, we've decided that
+            // setting the media player to null is an easy way to tell that the media player
+            // is not configured to play an audio file at the moment.
+            mMediaPlayer = null;
+        }
     }
 }
